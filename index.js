3bcd88bdff7130d3f2305451b4e033979ac79164
Ambar <Ambarpg80@gmail.com>
2022-09-26 23:30:37 -0400

    commit tree 2c0be0cfc9c9063568d11eacc6aec4e629aae65d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email){
+
+
+const newPost = { //Creates Post 
+    method: "POST",
+    headers:{
+       'Content-Type' : 'application/json',
+       'Accept':'application/json',
+          },
+    body: JSON.stringify({
+    name ,
+    email ,
+    }),
+    };
+
+return fetch('http://localhost:3000/users',newPost)
+.then(response => response.json())
+.then(resObject => document.body.innerHTML = resObject.id)//returned response Object
+.catch(error => document.body.innerHTML = error.message)
+ 
+}
+
+submitData();
+
+
+
+
+
+//function submitData(name){
+
+//}
+//submitData();
+/**/
